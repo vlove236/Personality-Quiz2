c0c98c6da7b34868346ffb934d7d17e7ffdc65c0
Love <ms.vlove718@gmail.com>
2020-09-27 09:32:48 -0500

    Initial Commit

---
+++ Personality Quiz2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Personality Quiz2
+//
+//  Created by MXC Swift on 9/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
